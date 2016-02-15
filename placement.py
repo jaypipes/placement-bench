5f6d4a59600a4a6dc28fbdf56bb0412313f38137
Jay Pipes <jaypipes@gmail.com>
2016-02-15 15:58:14 -0500

    Be more explicit about results stats
    Adds some niceness and specificity around the result structure and
    breaks the placement query into its own sub-function in the placement.py
    module, allowing easier understanding of where the timing and stats
    gathering is done around the placement and the claim areas of the run()
    method.

--- placement.py
+++ placement.py
@@ -24,18 +24,6 @@ def run(args, request_queue, result_queue, done_event):
     res = result.Result()
     res.process = worker_number
 
-    def drain_request_queue():
-        # Bleed out any remaining requests
-        unplaced_requests = 0
-        while True:
-            entry = request_queue.get()
-            request_queue.task_done()
-            if entry is not None:
-                unplaced_requests += 1
-            else:
-                break
-        LOG.info("Bled out %d unprocessed requests from queue." % unplaced_requests)
-
     def send_results():
         """
         Collates the results, pushes the results onto the result queue.
@@ -47,35 +35,20 @@ def run(args, request_queue, result_queue, done_event):
         conn.close()
         engine.dispose()
 
-    while True:
-        # Grab an entry from the request queue. Entries are a tuple of the form
-        # (uuid, resource_template).
-        entry = request_queue.get()
-        request_queue.task_done()
-        if entry is None:
-            LOG.info("No more entries in request queue after processing "
-                     "%d requests. Sending results." % res.query_count)
-            send_results()
-            return
-
-        instance_uuid = entry[0]
-        res_template = entry[1]
-        LOG.debug("Attempting to place instance %s request for %dMB RAM "
-                  "and %d vCPUs." %
-                  (instance_uuid,
-                   res_template[const.RAM_MB],
-                   res_template[const.VCPU]))
-
+    def get_selected_provider():
+        """
+        Return a resource provider that matches the requested resource
+        amounts.
+        """
         if args.schema == 'placement':
             select = db.placement_get_select(args, res_template, worker_number)
         
-        start_qry_time = time.time()
         records = conn.execute(select)
 
         if args.filter_strategy == 'db':
-            row = records.fetchone()
+            return records.fetchone()
         if args.filter_strategy == 'python':
-            filtered_nodes = []
+            filtered = []
             for row in records:
                 # Filter out by RAM
                 ram_available = (row['ram_total'] - row['ram_reserved']) * row['ram_allocation_ratio']
@@ -95,33 +68,54 @@ def run(args, request_queue, result_queue, done_event):
                     continue
                 if (cpu_available - cpu_used) < res_template[const.VCPU]:
                     continue
-                filtered_nodes.append(row)
-            if filtered_nodes:
+                filtered.append(row)
+            if filtered:
                 if args.placement_strategy == 'pack':
-                    filtered_nodes.sort(key=lambda t: (t['ram_used'], t['cpu_used'], -t['id']), reverse=True)
+                    filtered.sort(key=lambda t: (t['ram_used'], t['cpu_used'], -t['id']), reverse=True)
                 if args.placement_strategy == 'spread':
-                    filtered_nodes.sort(key=lambda t: (t['ram_used'], t['cpu_used'], t['id']))
+                    filtered.sort(key=lambda t: (t['ram_used'], t['cpu_used'], t['id']))
                 if args.placement_strategy == 'random':
-                    row = random.choice(filtered_nodes)
+                    return random.choice(filtered)
                 else:
-                    row = filtered_nodes[0]
-            else:
-                row = None
+                    return filtered[0]
 
-        qry_time = time.time() - start_qry_time
-        res.query_count += 1
-        res.min_query_time = min(res.min_query_time, qry_time)
-        res.max_query_time = max(res.max_query_time, qry_time)
-        res.total_query_time += qry_time
-
-        if not row:
-            LOG.info("No available space in datacenter for request for %d RAM and %d CPU" %
-                     (res_template[const.RAM_MB], res_template[const.VCPU]))
+    while True:
+        # Grab an entry from the request queue. Entries are a tuple of the form
+        # (uuid, resource_template).
+        entry = request_queue.get()
+        if entry is None:
+            LOG.info("No more entries in request queue after processing "
+                     "%d requests. Sending results." % res.requests_processed_count)
             send_results()
             return
 
-        provider_id = row[0]
-        generation = row[1]
+        res.requests_processed_count += 1
+
+        instance_uuid = entry[0]
+        res_template = entry[1]
+        LOG.debug("Attempting to place instance %s request for %dMB RAM "
+                  "and %d vCPUs." %
+                  (instance_uuid,
+                   res_template[const.RAM_MB],
+                   res_template[const.VCPU]))
+
+        start_placement_query_time = time.time()
+        selected = get_selected_provider()
+        res.add_placement_query_time(time.time() - start_placement_query_time)
+        res.placement_query_count += 1
+
+        if not selected:
+            LOG.info("No available space in datacenter for request for %d RAM and %d CPU" %
+                     (res_template[const.RAM_MB], res_template[const.VCPU]))
+            res.placement_no_found_provider_count += 1
+            send_results()
+            return
+
+        res.placement_found_provider_count += 1
+
+        provider_id = selected['id']
+        generation = selected['generation']
+
         start_trx_time = time.time()
         trans = conn.begin()
         try:
@@ -145,30 +139,28 @@ def run(args, request_queue, result_queue, done_event):
                     sql.and_(rp_tbl.c.id == provider_id,
                              rp_tbl.c.generation == generation)).values(generation=generation+1)
             upd_res = conn.execute(upd)
+            res.claim_trx_count += 1
+            
+            # Check to see if another thread executed a concurrent update on
+            # the same target resource provider
             rc = upd_res.rowcount
             if rc != 1:
-                # Another thread executed an allocation concurrently...
-                res.deadlock_count += 1
+                res.claim_deadlock_count += 1
                 trx_time = time.time() - start_trx_time
-                res.trx_count += 1
-                res.min_trx_time = min(res.min_trx_time, trx_time)
-                res.max_trx_time = max(res.max_trx_time, trx_time)
-                res.total_trx_time += trx_time
+                res.add_claim_trx_time(trx_time)
                 sleep_time = random.uniform(0.01, 0.10)
                 res.total_deadlock_sleep_time += sleep_time
                 time.sleep(sleep_time)
                 raise Exception("deadlocked, retrying transaction")
 
             trans.commit()
-            res.placement_count += 1
+
+            res.claim_success_count += 1
             trx_time = time.time() - start_trx_time
-            res.trx_count += 1
-            res.min_trx_time = min(res.min_trx_time, trx_time)
-            res.max_trx_time = max(res.max_trx_time, trx_time)
-            res.total_trx_time += trx_time
+            res.add_claim_trx_time(trx_time)
 
             LOG.debug("allocated instance %s to compute node %d. %06d RAM and %02d CPU." %
                       (instance_uuid, provider_id, res_template[const.RAM_MB], res_template[const.VCPU]))
         except Exception as e:
-            res.rollback_count += 1
+            res.claim_trx_rollback_count += 1
             trans.rollback()
--- result.py
+++ result.py
@@ -5,23 +5,40 @@ class Result(object):
 
     def __init__(self):
         self.process = None
-        self.total_trx_time = 0
-        self.total_query_time = 0
-        self.total_deadlock_sleep_time = 0
-        self.placement_count = 0
-        self.deadlock_count = 0
-        self.rollback_count = 0
-        self.trx_count = 0
-        self.query_count = 0
-        self.min_query_time = sys.maxint
-        self.max_query_time = 0
-        self.min_trx_time = sys.maxint
-        self.max_trx_time = 0
+        self.requests_processed_count = 0
+
+        # Placement query stats
+        self.placement_query_count = 0
+        self.placement_found_provider_count = 0
+        self.placement_no_found_provider_count = 0
+        self.placement_total_query_time = 0
+        self.placement_min_query_time = sys.maxint
+        self.placement_max_query_time = 0
+
+        # Claim transaction stats
+        self.claim_trx_count = 0
+        self.claim_success_count = 0
+        self.claim_deadlock_count = 0
+        self.claim_trx_rollback_count = 0
+        self.claim_total_deadlock_sleep_time = 0
+        self.claim_total_trx_time = 0
+        self.claim_min_trx_time = sys.maxint
+        self.claim_max_trx_time = 0
+
+    def add_placement_query_time(self, time):
+        self.placement_min_query_time = min(self.placement_min_query_time, time)
+        self.placement_max_query_time = max(self.placement_max_query_time, time)
+        self.placement_total_query_time += time
+
+    def add_claim_trx_time(self, time):
+        self.claim_min_trx_time = min(self.claim_min_trx_time, time)
+        self.claim_max_trx_time = max(self.claim_max_trx_time, time)
+        self.claim_total_trx_time += time
 
     @property
-    def avg_trx_time(self):
-        return self.tot_trx_time / float(self.trx_count)
+    def claim_avg_trx_time(self):
+        return self.claim_total_trx_time / float(self.claim_trx_count)
 
     @property
-    def avg_query_time(self):
-        return self.tot_query_time / float(self.query_count)
+    def placement_avg_query_time(self):
+        return self.placement_total_query_time / float(self.placement_query_count)
